9d3d3bd187a56f50c0314de4efca01fc6ffd9572
codem4ker <misiewicz.michal@outlook.com>
2017-04-08 21:17:13 +0200

    Dodano AE i prosty test
    TODO:
    - rozwinięcie funcji evaluateFitness
    - lepsze skalowanie współczynnika dostosowania
    - masa innych drobnych poprawek

---
+++ src/pszt/ga/exceptions/ParentSizeException.java
@@ -0,0 +1,8 @@
+package pszt.ga.exceptions;
+
+/**
+ * Created by Michał on 08-04-2017.
+ */
+public class ParentSizeException extends Exception
+{
+}
---
+++ src/pszt/ga/exceptions/ProbabilityException.java
@@ -0,0 +1,8 @@
+package pszt.ga.exceptions;
+
+/**
+ * Created by Michał on 07-04-2017.
+ */
+public class ProbabilityException extends Exception
+{
+}
