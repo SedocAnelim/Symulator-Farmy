10fc26305704cb48d7c63653cf1cb59c69c18d78
SedocAnelim <chockowhite26@gmail.com>
2017-05-07 00:31:31 +0200

    Update Main.java

--- src/pszt/farmy/symulator/Main.java
+++ src/pszt/farmy/symulator/Main.java
@@ -1,21 +1,49 @@
 package pszt.farmy.symulator;
 
+
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import pszt.ga.exceptions.ElitismException;
+import pszt.ga.exceptions.ParentSizeException;
+import pszt.ga.exceptions.ProbabilityException;
+import pszt.test.*;
+
 
 public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Symulator farmy");
-        primaryStage.setScene(new Scene(root, 800, 500));
-        primaryStage.show();
-    }
-
+	
+	Stage window;
+	Scene scene1, scene2;
+	
+	
+	 @Override
+	    public void start(Stage primaryStage) {
+		 
+		 window = primaryStage;
+		 
+		 Button test1Button = new Button();
+		 test1Button.setText("New test");
+		 test1Button.setOnAction(e -> {
+			 try {
+				Test.main();
+			} catch (ProbabilityException | ElitismException | ParentSizeException e1) {
+				e1.printStackTrace();
+			}
+			 Chart.display(Test.getDataList());
+		 });
+		 
+		 StackPane layout1 = new StackPane();	
+		 layout1.getChildren().addAll(test1Button);
+		 scene1 = new Scene(layout1, 200, 200);
+		 
+		 window.setScene(scene1);
+		 window.setTitle("Symulator Farmy");
+		 window.show();
+	 }
+		 
+	
 
     public static void main(String[] args) {
         launch(args);
